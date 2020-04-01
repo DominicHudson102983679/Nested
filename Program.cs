ad108f5ca5f66364d8aa92416f076a49d4dcc533
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-04-01 17:36:25 +1100

    Nested Repitition

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Nested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------------------Exercise 1.1   
+            // counter = 0
+            // get name
+
+            //while name < 10
+            //  print name
+            //  counter +1
+
+            // int counter = 0;
+
+            // Console.Write("enter name: ");
+            // string name = Console.ReadLine();
+
+            //   while (counter < 10)
+            //       {
+            //        Console.Write(name + " ");
+            //        counter++;
+            //       }
+
+            //        Console.ReadLine();
+
+
+
+
+
+            //----------------------------------------Exercise 1.2
+
+            //get name
+
+            //get count
+            //set count
+
+            //while count is above zero
+            //print name
+            //minus 1 from count value
+
+
+            // Console.Write("Enter Name: ");
+            // string name = Console.ReadLine();
+
+            // Console.Write("Enter Amount: ");
+            // int counter = Convert.ToInt32(Console.ReadLine());
+
+            // while (counter > 0)
+            // {
+            //    Console.WriteLine(name);
+            //    counter--;
+            // };
+
+
+            //---------------------------------------Exercise 2.1
+
+            // string name = "x";
+
+            // int counter1 = 0;
+
+            // while (counter1 <= 5) {
+            //    counter1++;
+
+            //    int counter2 = counter1;
+
+            //    while (counter2 <= 5)
+            //    {
+            //        Console.Write(name);
+
+            //        counter2++;
+            //    }
+
+            //    Console.WriteLine();    
+        }
+    }
+}
